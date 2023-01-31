a0704d4aefa7d37e92e90a4761f9a581bf2f3429
Espadane <52450374+Espadane@users.noreply.github.com>
2023-02-01 01:43:11 +0300

    logger update

--- flibusta_logger.py
+++ flibusta_logger.py
@@ -1,10 +1,14 @@
 import logging
 
+PROJECT_NAME = 'Flibusta bot'
+
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.WARNING)
 logger_handler = logging.FileHandler('flibusta_bot.log')
 logger_formatter = logging.Formatter(
-    '%(asctime)s - %(levelname)s - %(funcName)s: %(lineno)d - %(message)s')
+    f'{PROJECT_NAME}: %(asctime)s - %(levelname)s - \
+%(funcName)s: %(lineno)d - %(message)s', 
+    datefmt='%d.%b.%Y %H:%M')
 logger.addHandler(logger_handler)
 logger_handler.setFormatter(logger_formatter)
